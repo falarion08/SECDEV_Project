c2aa4574d9c32654f3ba96d7173ba473083f85d0
Kevin Abelgas <kev.abelgas@gmail.com>
2024-02-13 05:29:22 +0800

    routes fix
    - fixed issue where all routes resulted in 404

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,6 +4,8 @@ from flask import Blueprint
 main_bp = Blueprint('main', __name__)
 # admin_bp = Blueprint('admin', __name__)
 
+# do not remove this, it will cause all routes to result in 404 error
+from . import main
 
 def register_blueprints(app):
     app.register_blueprint(main_bp)
