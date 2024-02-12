23c78fa4a71bf21866f31270ac1b5100ea85fde6
Kevin Abelgas <kev.abelgas@gmail.com>
2024-02-13 01:57:04 +0800

    update frontend
    - update base, login and register html
    - update routes
    - use jinja2 templating an
    - add navbar
    - add index page

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from app.models.user import db
-from app.routes.main import main_bp
 from app.routes import register_blueprints
 from dotenv import load_dotenv
 import os
@@ -13,17 +12,17 @@ def create_app():
     app = Flask(__name__)
 
     # Connect to the database hosted online
-    DB_URL = os.environ.get("DATABASE_URL")
-    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Set to false to use less memory
-    app.config['SQLALCHEMY_ECHO'] = True
+    #DB_URL = os.environ.get("DATABASE_URL")
+    #app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Set to false to use less memory
+    #app.config['SQLALCHEMY_ECHO'] = True
 
-    db.init_app(app)
+    #db.init_app(app)
 
     register_blueprints(app)
 
     # Create tables that does not exist in the database
     app.app_context().push()
-    db.create_all()
+    #db.create_all()
 
     return app
