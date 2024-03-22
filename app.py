d2089af6a102754d9a7782c1d1f64b3773ca6b18
falarion08 <barrion.ryanonil@gmail.com>
2024-03-22 13:12:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    # For deployment
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     # For deployment
+#     app.run(debug=True, host = "0.0.0.0")
     
-    # For demo
-    # app.run(debug = True, ssl_context =('cert.pem','key.pem'))
+#     # For demo
+#     # app.run(debug = True, ssl_context =('cert.pem','key.pem'))
     
