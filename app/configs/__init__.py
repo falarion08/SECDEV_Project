84694e29fe243f9f0e630d2ab966415995f9df3e
falarion08 <barrion.ryanonil@gmail.com>
2024-03-22 10:03:29 +0800

    Updated .env files

--- app/configs/__init__.py
+++ app/configs/__init__.py
@@ -3,15 +3,15 @@ from datetime import timedelta
 from flask.logging import default_handler
 
 def setup_configs(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345@127.0.0.1:5432/CSSECDEV"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("ONLINE_DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Set to false to use less memory
     #app.config['SQLALCHEMY_ECHO'] = True
     app.config['SECRET_KEY'] = os.urandom(24)
     app.config['UPLOAD_FOLDER'] = os.getenv('FOLDER_UPLOAD')
-    app.config['MAX_CONTENT_LENGTH'] = int(os.getenv('MAX_IMAGE_SIZE')) # 1MB Max
+    app.config['MAX_CONTENT_LENGTH'] =int(os.getenv("MAX_FILE_UPLOAD_SIZE")) # 5MB Max
     app.config['RECAPTCHA_USE_SSL']= False
-    app.config['RECAPTCHA_PUBLIC_KEY']= "6LcWx3MpAAAAAEM6Zce08cRhLriIcos5dI5YFpcE"
-    app.config['RECAPTCHA_PRIVATE_KEY']="6LcWx3MpAAAAAPP_N_-NfwWhPeTPxyKHlM_60DTV"
+    app.config['RECAPTCHA_PUBLIC_KEY']= os.getenv("RECAPTCHA_PUBLIC_KEY")
+    app.config['RECAPTCHA_PRIVATE_KEY']=os.getenv("RECAPTCHA_PRIVATE_KEY")
     app.config['RECAPTCHA_OPTIONS'] = {'theme':'white'}
     app.config['SESSION_COOKIE_SECURE'] = True
     app.config['SESSION_COOKIE_HTTPONLY'] = True
