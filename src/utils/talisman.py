6ebc7f4c6c7f6ca39be1ce0032c62f440525cd54
Kevin Abelgas <kev.abelgas@gmail.com>
2024-10-09 00:21:37 +0800

    fix: csp for fonts and recaptcha
    test: added talisman and updated csp

--- src/utils/talisman.py
+++ src/utils/talisman.py
@@ -2,17 +2,26 @@
 SELF = "\'self\'"
 
 csp = {
-   'default-src': ['\'self\'',
-                   'https://www.google.com/recaptcha/api.js',
-                    'https://www.gstatic.com/'],
-    'script-src': ['\'self\'',
-                   'https://www.google.com/recaptcha/api.js',
-                   ' https://www.gstatic.com/'],
-    'frame-src':['\'self\'',
-                 'https://www.google.com/recaptcha/api.js',
-                   ' https://www.gstatic.com/']
-
-
+  'default-src': [
+    '\'self\'',
+  ],
+  'script-src': [
+    '\'self\'',
+    'https://www.google.com',
+    'https://www.gstatic.com/'
+  ],
+  'frame-src': [
+    'https://www.google.com',
+  ],
+  'font-src': [
+    '\'self\'',
+    'https://fonts.googleapis.com', 
+    'https://fonts.gstatic.com'
+  ],
+  'style-src': [
+    '\'self\'',
+    'https://fonts.googleapis.com'
+  ]
 }
 
 # HTTP Strict Transport Security (HSTS) Header
