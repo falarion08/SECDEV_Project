53b246041baa9aed94f5c8c1a24847a61876974a
Kevin Abelgas <kev.abelgas@gmail.com>
2024-10-09 22:44:35 +0800

    fix: use database_url directly

--- src/configs/__init__.py
+++ src/configs/__init__.py
@@ -6,8 +6,8 @@ def setup_configs(app):
     database_url = os.getenv("DATABASE_URL")
     if database_url and database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
-        
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(database_url)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Set to false to use less memory
     app.config['SQLALCHEMY_ECHO'] = True
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
